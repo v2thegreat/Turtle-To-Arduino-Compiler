354508821bb924cf3022115240199e1700177b47
v2thegreat <aliabbaszaidi@outlook.com>
2018-01-15 17:44:01 -0500

    Add files via upload

---
+++ Compiler/Compiler-2/Output Arduino Code.ino
@@ -0,0 +1,85 @@
+int MovePin=2;
+int TurnPin=3;
+int PenLEDPin=4;
+int VisibilityPin=5;
+
+int DistanceDelay=1;
+int TurnDelay=1;
+
+void setup()
+{
+	pinMode(MovePin, OUTPUT);
+	pinMode(TurnPin, OUTPUT);
+}
+
+void loop()
+{
+	secondsDelay(1);
+	forward(5);
+	rightTurn(9);
+	secondsDelay(1);
+}
+
+void secondsDelay(int s)
+{
+	delay(1000*s);
+	resetBug();
+}
+
+void penup()
+{
+	digitalWrite(PenLEDPin, LOW);
+}
+
+void pendown()
+{
+	digitalWrite(PenLEDPin, HIGH);
+}
+
+void showTurtle()
+{
+	digitalWrite(VisibilityPin, HIGH);
+}
+
+void hideTurtle()
+{
+	digitalWrite(VisibilityPin, LOW);
+}
+
+void forward(int distance)
+{
+	digitalWrite(MovePin, HIGH);
+	secondsDelay(DistanceDelay*distance);
+	digitalWrite(MovePin, LOW);
+}
+
+void leftTurn(int angle)
+{
+	digitalWrite(TurnPin, HIGH);
+	secondsDelay(TurnDelay*angle);
+	digitalWrite(TurnPin, LOW);
+}
+
+void rightTurn(int angle)
+{
+	angle=360-angle;
+	leftTurn(angle);
+}
+
+void turnOnly()
+{
+	digitalWrite(MovePin, HIGH);
+	digitalWrite(TurnPin, LOW);
+}
+
+void moveOnly()
+{
+	digitalWrite(MovePin, LOW);
+	digitalWrite(TurnPin,HIGH);
+}
+
+void resetBug()
+{
+	digitalWrite(MovePin, LOW);
+	digitalWrite(TurnPin, LOW);
+}
