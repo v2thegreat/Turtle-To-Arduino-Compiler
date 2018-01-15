c037e0843fcc1618dd5845fd6938164d3682abfe
v2thegreat <aliabbaszaidi@outlook.com>
2018-01-15 17:42:59 -0500

    Compiler Source Code
    Version 1.0 Source Code

---
+++ Compiler/Compiler-1/Compiler.py
@@ -0,0 +1,108 @@
+import pickle
+
+class turtleCommandsClass():
+
+	#Getting Information
+	intCommandsFull=pickle.load(open('intCommandsFull.txt','rb'))
+	intCommandsShort=pickle.load(open('intCommandsShort.txt','rb'))
+	nonValueCommands=pickle.load(open('nonValueCommands.txt','rb'))
+	arduinoCode = pickle.load(open('arduinoCode.txt','rb'))
+
+
+	def __init__(self, turtleCode=''):
+		self.turtleCode=turtleCode.lower() #Converting the entire code to lowercase
+		self.__formatCommands()		#Converting the commands to work better
+
+
+	def __formatCommands(self):
+		l=self.turtleCode.split() #Getting individual words
+		self.turtleCommandsLst=[] #Creating a list to save commands
+		for x in enumerate(l):
+			self.__checkTurtleSyntax(x[1]) #Checking each items format and raising errors when they're incorrect
+
+			#Checking where each item belongs and how to run it
+			
+			#Checking if the item belongs to int commands list (if it was one above or below)
+			if x[1] in turtleCommandsClass.intCommandsFull or x[1] in turtleCommandsClass.intCommandsShort:
+				self.turtleCommandsLst.append('{0} {1}'.format(x[1], l[x[0]+1]))
+
+			#Same thing here
+			if x[1] in turtleCommandsClass.nonValueCommands:
+				self.turtleCommandsLst.append(x[1])
+
+			#continuing if the item is an interger
+			if x[1].isdigit() or x[1].isdecimal():
+				continue
+
+
+	def __checkTurtleSyntax(self, command):
+		#Checking if command belongs in intCommandsShort
+		ch1 = command in turtleCommandsClass.intCommandsShort
+
+		#Checking if command belongs in intCommandsFull
+		ch2 = command in turtleCommandsClass.intCommandsFull
+
+		#Checking if command belongs in nonValueCommands
+		ch3 = command in turtleCommandsClass.nonValueCommands
+
+		#Checking if the command is actually an integer
+		ch4 = command.isdigit() or command.isdecimal()
+
+		#If any of these conditions are true, and raises if none of them work
+		if (ch1 or ch2 or ch3 or ch4):
+			return None
+		else:
+			raise SyntaxError('Invalid Command: {0} not understood'.format(command))
+
+
+	#Method for converting mainloop code to C++ arduino Code
+	def getArduinoLoopCode(self):
+		code=''
+		for x in self.turtleCommandsLst:
+
+			#Assigning Boolean Values to check how they work
+			isIn_intCommandsFull=x.split()[0] in turtleCommandsClass.intCommandsFull
+			isIn_intCommandsShort=x.split()[0] in turtleCommandsClass.intCommandsShort
+			isIn_nonValueCommands=x.split()[0] in turtleCommandsClass.nonValueCommands
+
+			if isIn_intCommandsFull or isIn_intCommandsShort:
+				code += turtleCommandsClass.arduinoCode[x.split()[0]] + '({0});\n\t'.format(x.split()[1])
+
+			if isIn_nonValueCommands:
+				code += turtleCommandsClass.arduinoCode[x.split()[0]]+'();\n\t'
+
+		return code[:-2]
+
+
+	def getArduinoCode(self):
+		arduinoCode = pickle.load(open('arduinoSourceCode--To Mod.txt','rb'))
+		arduinoCode = arduinoCode.replace("//1650--TENNIS",self.getArduinoLoopCode())
+		return arduinoCode
+
+
+	def saveArduinoCode(self, fileName='arduino Code.ino'):
+		open(fileName,'w').write(self.getArduinoCode())
+
+
+	def saveChangesToSourceCode(self, modifiedSourceCode):
+		Warning('This has not been tested yet')
+		Warning('''Ensure that the lines: '//1650--TENNIS' is present in the comments
+				That line is used as a replacement placeholder for easy formatting''')
+		
+		if '//1650--TENNIS' not in modifiedSourceCode:
+			raise ValueError("Command: //1650--TENNIS not found")
+
+		data=open(modifiedSourceCode, 'r').read()
+		pickle.dump(data, open("arduinoSourceCode--To Mod.txt",'wb'))
+
+
+	def getSourceCode(self):
+		return pickle.load(open("arduinoSourceCode--To Mod.txt",'rb'))
+
+
+def main():
+	s='fd 50 rt 90'				#input('Enter the Turtle Command')
+	t=turtleCommandsClass(turtleCode=s).saveArduinoCode()
+
+if __name__ == '__main__':
+	main()
---
+++ Compiler/Compiler-1/arduino Code.ino
@@ -0,0 +1,83 @@
+void setup()
+{
+	MovePin=2;
+	TurnPin=3;
+	PenLEDPin=4;
+	VisibilityPin=5;
+
+	DistanceDelay=1;
+	TurnDelay=1;
+
+	pinMode(MovePin, OUTPUT);
+	pinMode(TurnPin, OUTPUT);
+}
+
+void loop()
+{
+	forward(50);
+	rightTurn(90);
+}
+
+void secondsDelay(int s)
+{
+	delay(1000*s);
+	resetBug();
+}
+
+void penup()
+{
+	digitalWrite(PenLEDPin, LOW);
+}
+
+void pendown()
+{
+	digitalWrite(PenLEDPin, HIGH);
+}
+
+void showTurtle()
+{
+	digitalWrite(VisibilityPin, HIGH);
+}
+
+void hideTurtle()
+{
+	digitalWrite(VisibilityPin, LOW);
+}
+
+void forward(int distance)
+{
+	digitalWrite(MovePin, HIGH);
+	secondsDelay(DistanceDelay*distance)
+	digitalWrite(MovePin, LOW);
+}
+
+void leftTurn(int angle)
+{
+	digitalWrite(TurnPin, HIGH);
+	secondsDelay(TurnDelay*angle)
+	digitalWrite(TurnPin, LOW);
+}
+
+void rightTurn(int angle)
+{
+	angle=360-angle;
+	leftTurn(angle);
+}
+
+void turnOnly()
+{
+	digitalWrite(MovePin, HIGH);
+	digitalWrite(TurnPin, LOW);
+}
+
+void moveOnly()
+{
+	digitalWrite(MovePin, LOW);
+	digitalWrite(TurnPin,HIGH);
+}
+
+void resetBug()
+{
+	digitalWrite(MovePin, LOW);
+	digitalWrite(TurnPin, LOW);
+}
