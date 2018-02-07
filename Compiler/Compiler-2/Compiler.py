327875c3bca161e518b8af65ebe4305994a6cdca
v2thegreat <aliabbaszaidi@outlook.com>
2018-02-07 12:15:22 -0500

    Refactored Code
    Removed redundancies, modified variables and re-wrote parts of code to enahnce readability and ease of modifications and add- ons in the future

--- Compiler/Compiler-2/Compiler.py
+++ Compiler/Compiler-2/Compiler.py
@@ -1,13 +1,7 @@
-import pickle
+from commands import intCommandsFull, intCommandsShort, nonValueCommands, arduinoCode
+import sourceCode
 
 class turtleCommandsClass():
-
-	#Getting Information
-	intCommandsFull=pickle.load(open('intCommandsFull.txt','rb')) #List for all Commands in Full -> ['forward', 'left', 'right', 'setx', 'sety', 'setxy', 'setheading', 'arc']
-	intCommandsShort=pickle.load(open('intCommandsShort.txt','rb')) #List for all Commands in short -> ['fd', 'lt', 'rt', 'seth']
-	nonValueCommands=pickle.load(open('nonValueCommands.txt','rb')) #List for all non value based Commands -> ['penup', 'pendown', 'hideturtle', 'showturtle', 'home']
-	arduinoCode = pickle.load(open('arduinoCode.txt','rb')) #Dictionary for all ()-> {'forward': 'forward', 'fd': 'forward', 'right': 'rightTurn', 'rt': 'rightTurn', 'left': 'leftTurn', 'lt': 'leftTurn', 'pendown': 'pendown', 'penup': 'penup', 'hideturtle': 'hideturtle', 'showturtle': 'showturtle'}
-
 
 	def __init__(self, turtleCode=''):
 		self.turtleCode=turtleCode.lower() #Converting the entire code to lowercase
@@ -23,11 +17,11 @@ class turtleCommandsClass():
 			#Checking where each item belongs and how to run it
 			
 			#Checking if the item belongs to int commands list (if it was one above or below)
-			if x[1] in turtleCommandsClass.intCommandsFull or x[1] in turtleCommandsClass.intCommandsShort:
+			if x[1] in intCommandsFull or x[1] in intCommandsShort:
 				self.turtleCommandsLst.append('{0} {1}'.format(x[1], l[x[0]+1]))
 
 			#Same thing here
-			if x[1] in turtleCommandsClass.nonValueCommands:
+			if x[1] in nonValueCommands:
 				self.turtleCommandsLst.append(x[1])
 
 			#continuing if the item is an interger
@@ -37,13 +31,13 @@ class turtleCommandsClass():
 
 	def __checkTurtleSyntax(self, command):
 		#Checking if command belongs in intCommandsShort
-		ch1 = command in turtleCommandsClass.intCommandsShort
+		ch1 = command in intCommandsShort
 
 		#Checking if command belongs in intCommandsFull
-		ch2 = command in turtleCommandsClass.intCommandsFull
+		ch2 = command in intCommandsFull
 
 		#Checking if command belongs in nonValueCommands
-		ch3 = command in turtleCommandsClass.nonValueCommands
+		ch3 = command in nonValueCommands
 
 		#Checking if the command is actually an integer
 		ch4 = command.isdigit() or command.isdecimal()
@@ -61,23 +55,24 @@ class turtleCommandsClass():
 		for x in self.turtleCommandsLst:
 
 			#Assigning Boolean Values to check how they work
-			isIn_intCommandsFull=x.split()[0] in turtleCommandsClass.intCommandsFull
-			isIn_intCommandsShort=x.split()[0] in turtleCommandsClass.intCommandsShort
-			isIn_nonValueCommands=x.split()[0] in turtleCommandsClass.nonValueCommands
+			isIn_intCommandsFull=x.split()[0] in intCommandsFull
+			isIn_intCommandsShort=x.split()[0] in intCommandsShort
+			isIn_nonValueCommands=x.split()[0] in nonValueCommands
 
 			if isIn_intCommandsFull or isIn_intCommandsShort:
-				code += turtleCommandsClass.arduinoCode[x.split()[0]] + '({0});\n\t'.format(x.split()[1])
+				code += arduinoCode[x.split()[0]] + '({0});\n\t'.format(x.split()[1])
 
 			if isIn_nonValueCommands:
-				code += turtleCommandsClass.arduinoCode[x.split()[0]]+'();\n\t'
+				code += arduinoCode[x.split()[0]]+'();\n\t'
 
 		return code[:-2]
 
 
 	def getArduinoCode(self):
-		arduinoCode = pickle.load(open('arduinoSourceCode--To Mod.txt','rb'))
-		arduinoCode = arduinoCode.replace("//1650--TENNIS",self.getArduinoLoopCode())
-		return arduinoCode
+		arduinoSourceCode = sourceCode.getArduinoSourceCode
+		arduinoLoopCode = self.getArduinoLoopCode()
+		arduinoSourceCode = arduinoSourceCode.replace("//1650--TENNIS", arduinoLoopCode)
+		return arduinoSourceCode
 
 
 	def saveArduinoCode(self, fileName='Output Arduino Code.ino'):
@@ -88,16 +83,12 @@ class turtleCommandsClass():
 		Warning('This has not been tested yet')
 		Warning('''Ensure that the lines: '//1650--TENNIS' is present in the comments
 				That line is used as a replacement placeholder for easy formatting''')
-		
+
 		if '//1650--TENNIS' not in modifiedSourceCode:
 			raise ValueError("Command: //1650--TENNIS not found")
 
 		data=open(modifiedSourceCode, 'r').read()
 		pickle.dump(data, open("arduinoSourceCode--To Mod.txt",'wb'))
-
-
-	def getSourceCode(self):
-		return pickle.load(open("arduinoSourceCode--To Mod.txt",'rb'))
 
 
 def main():
---
+++ Compiler/Compiler-2/commands.py
@@ -0,0 +1,10 @@
+intCommandsFull=['forward', 'left', 'right', 'setx', 'sety', 'setxy', 
+				'setheading', 'arc']
+
+intCommandsShort=['fd', 'lt', 'rt', 'seth']
+
+nonValueCommands=['penup', 'pendown', 'hideturtle', 'showturtle', 'home']
+arduinoCode={'forward': 'forward', 'fd': 'forward', 'right': 'rightTurn',
+ 			'rt': 'rightTurn', 'left': 'leftTurn', 'lt': 'leftTurn', 
+ 			'pendown': 'pendown', 'penup': 'penup', 'hideturtle': 'hideturtle', 
+ 			'showturtle': 'showturtle'}
